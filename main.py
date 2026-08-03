ba2084755dc9f49ddaeeaf3192b020e8cd409643
adi542 <adityakkochhar1@gmail.com>
2026-08-03 16:44:48 +0530

    main app code restored after test

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# from fastapi import FastAPI
-# from app.api.routes.document_routes import router as document_router
-
-# app = FastAPI(
-  # title="DocuMind AI",
-  # version="1.0.0",
-# )
-
-# app.include_router(document_router)
-
-
 from fastapi import FastAPI
+from app.api.routes.document_routes import router as document_router
 
-app = FastAPI()
+app = FastAPI(
+  title="DocuMind AI",
+  version="1.0.0",
+)
 
-@app.get("/")
-def home():
-    return {"status": "working"}
+app.include_router(document_router)
